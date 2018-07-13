29c101a1b098f0c8c9ea6d47f7fef1e85e92ea60
Vlad Malikhtarovich <royalfint@hotmail.com>
2018-07-13 01:51:19 +0000

    call

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(function(req, res, next){
    res.locals.currentUser = req.user;
    res.locals.error = req.flash("error");
    res.locals.success = req.flash("success");
+   res.locals.url = "https://bazarlar--royalfint.c9users.io";
    next();
 });
 
